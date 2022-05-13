0923a261f0a5438f38781125d9f7ed18602fd06e
temavedernikov <vedernikov.tema1996@gmail.com>
2022-05-13 08:20:12 +0500

    Add settings constants

--- settings.py
+++ settings.py
@@ -10,9 +10,29 @@ class Settings(BaseSettings):
     )
     REDIS_HOST: str = Field(..., description="Хост для подключения redis")
     REDIS_PORT: int = Field(..., description="Порт для подключения redis")
-    REDIS_DB: int = Field(..., description="Бд для подключения redis")
+    REDIS_DB: int = Field(..., description="Адрес для подключения redis")
+    SECRET_KEY: str = Field(..., description="Секретный ключ проекта")
+    ACCESS_TOKEN_EXPIRE_DAYS: int = Field(
+        ..., description="Время действия токена"
+    )
+    ALGORITHM: str = Field(..., description="Алгоритм создания токена")
+    ORIGINS: List[str] = Field(
+        ..., description="Список доступных адресов(CORS)"
+    )
+    USER_DEFAULT_ROLE: int = Field(
+        ..., description="Стандартное значение роли пользователя"
+    )
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
         env_prefix = "BN_"
+
+
+settings = Settings()
+
+
+def get_settings():
+    global settings
+    settings = Settings() if not settings else settings
+    return settings
