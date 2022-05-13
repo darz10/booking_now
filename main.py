5fb00dfa1521d36946736e6f9d68ef5a15b616b5
temavedernikov <vedernikov.tema1996@gmail.com>
2022-05-13 08:22:51 +0500

    Change main file

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from asyncio import get_event_loop
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import database
 from settings import settings
+from accounts.views import user, authorization
 
 app = FastAPI()
 
+db = database.BookingDataBase(settings.DB_CONNECT)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.origins,
+    allow_origins=settings.ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -17,5 +20,11 @@ app.add_middleware(
 
 @app.on_event("startup")
 async def startup():
-    app.state.settings = settings
-    app.state.loop = get_event_loop()
+    app.settings = settings
+    app.loop = get_event_loop()
+    app.db = db
+    await db.get_asyncpg_pool()
+
+
+app.include_router(user.router)
+app.include_router(authorization.router)
