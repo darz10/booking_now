29f7360890bfaa34efb0101a41b19930e420c0c2
temavedernikov <vedernikov.tema1996@gmail.com>
2022-07-03 13:29:46 +0500

    Add routers in main file

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from asyncio import get_event_loop
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import database
+from database.db import create_db
 from settings import settings
 from accounts.views import user, authorization
+from institutions.views import (
+    place, 
+    place_branch, 
+    country, 
+    city, 
+    address,
+    user_place,
+    table,
+    reservation,
+    media_file,
+    place_media_file
+)
+
 
 app = FastAPI()
 
-db = database.BookingDataBase(settings.DB_CONNECT)
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,9 +34,18 @@ app.add_middleware(
 async def startup():
     app.settings = settings
     app.loop = get_event_loop()
-    app.db = db
-    await db.get_asyncpg_pool()
+    await create_db()
 
 
 app.include_router(user.router)
 app.include_router(authorization.router)
+app.include_router(place.router)
+app.include_router(place_branch.router)
+app.include_router(country.router)
+app.include_router(city.router)
+app.include_router(address.router)
+app.include_router(user_place.router)
+app.include_router(table.router)
+app.include_router(reservation.router)
+app.include_router(media_file.router)
+app.include_router(place_media_file.router)
