7bd3fec8e9942fb18f2c1e549a80aa401152f81a
temavedernikov <vedernikov.tema1996@gmail.com>
2022-11-22 22:32:15 +0400

    Change connection db to async

--- database/db.py
+++ database/db.py
@@ -1,17 +1,17 @@
-from sqlalchemy import inspect
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
+import databases
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
 
 from settings import settings
 
+
 SQLALCHEMY_DATABASE_URL = settings.DB_CONNECT
 
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL, future=True, echo=True)
-async_session = sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+)
+
+database = databases.Database(SQLALCHEMY_DATABASE_URL)
+
 
 Base = declarative_base()
-
-
-async def create_db() -> None:
-    async with engine.connect() as conn:
-        await conn.run_sync(Base.metadata.create_all)
