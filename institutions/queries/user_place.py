d0283835109115ba10b6bc54775c7aa742da0ecb
temavedernikov <vedernikov.tema1996@gmail.com>
2022-11-29 21:36:21 +0400

    Change update method for UserPlaceRepository

--- institutions/queries/user_place.py
+++ institutions/queries/user_place.py
@@ -13,14 +13,14 @@ class UserPlaceRepository(AbstractRepository):
         self.db_connection = db_connection
         self.db_model = db_model
 
-    async def create(self, user_id: int, place_branch_id: int, 
-                     is_favourite: Optional[bool]=None) -> Record:
+    async def create(self, user_id: int, place_branch_id: int,
+                     is_favourite: Optional[bool] = None) -> Record:
         """Создание связи пользователя с местом"""
         user_place = insert(self.db_model).values(
             user_id=user_id,
             place_branch_id=place_branch_id,
             is_favourite=is_favourite
-        ).returning(self.db_model)        
+        ).returning(self.db_model)
         return await self.db_connection.fetch_one(user_place)
 
     async def all(self) -> List[Record]:
@@ -37,18 +37,13 @@ class UserPlaceRepository(AbstractRepository):
         user_place = update(self.db_model).\
                      where(self.db_model.id == id).\
                      returning(self.db_model)
-        if kwargs.get("place_id"):
-            user_place = user_place.values(place_id=kwargs["place_id"])
-        if kwargs.get("place_address_id"):
-            user_place = user_place.values(place_address_id=kwargs["place_address_id"])
-        if kwargs.get("is_favourite"):
-            user_place = user_place.values(place_address_id=kwargs["place_address_id"])
+        user_place = user_place.values(**kwargs)
         return await self.db_connection.fetch_one(user_place)
 
     async def delete(self, id: int) -> None:
         """Удаление адреса с возвратом статуса, True(объект удалён)"""
         user_place = delete(self.db_model).\
-                     where(self.db_model.id == id).\
-                     returning(self.db_model.id)
+            where(self.db_model.id == id).\
+            returning(self.db_model.id)
         deleted_user_place = await self.db_connection.execute(user_place)
         return deleted_user_place
