0cd5b959f172dcd956b1efa5cea456df1b93b636
temavedernikov <vedernikov.tema1996@gmail.com>
2022-11-29 21:33:22 +0400

    Change update method for MediaFileRepository

--- institutions/queries/media_file.py
+++ institutions/queries/media_file.py
@@ -17,7 +17,7 @@ class MediaFileRepository(AbstractRepository):
     async def create(self, source_id: str, user_id: int,
                      source_url: str, uploaded: bool, created_at: datetime,
                      filename: str, source_fields: Optional[dict]) -> Record:
-        
+
         new_media_file = insert(self.db_model).values(
             source_id=source_id,
             source_fields=source_fields,
@@ -43,24 +43,13 @@ class MediaFileRepository(AbstractRepository):
         media_file = update(self.db_model).\
                      where(self.db_model.id == id).\
                      returning(self.db_model)
-        if kwargs.get("source_id"):
-            media_file = media_file.values(source_id=kwargs["source_id"])
-        if kwargs.get("source_fields"):
-            media_file = media_file.values(source_fields=kwargs["source_fields"])
-        if kwargs.get("source_url"):
-            media_file = media_file.values(source_url=kwargs["source_url"])
-        if kwargs.get("uploaded"):
-            media_file = media_file.values(uploaded=kwargs["uploaded"])
-        if kwargs.get("created_at"):
-            media_file = media_file.values(created_at=kwargs["created_at"])
-        if kwargs.get("filename"):
-            media_file = media_file.values(filename=kwargs["filename"])
-        if kwargs.get("user_id"):
-            media_file = media_file.values(user_id=kwargs["user_id"])
+        media_file = media_file.values(**kwargs)
         return await self.db_connection.fetch_one(media_file)
 
     async def delete(self, id: int) -> None:
         """Удаление стола возвратом статуса, True(объект удалён)"""
-        media_file = delete(self.db_model).where(self.db_model.id == id).returning(self.db_model.id)
+        media_file = delete(self.db_model).where(
+            self.db_model.id == id
+        ).returning(self.db_model.id)
         deleted_media_file = await self.db_connection.execute(media_file)
         return deleted_media_file
